b7869f8e53c926cd9af2dc2019b11463701fe6c0
ChandraShekhar <csyadav0513@gmail.com>
2024-11-18 23:10:36 +0530

    refactor AnimeCard component to use framer-motion for animations; update fetchAnime to return AnimeCard elements; create MotionDiv component for animated divs

--- app/action.tsx
+++ app/action.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use server";
 
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+
 export const fetchAnime = async (page: number) => {
   const response = await fetch(
     `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
@@ -13,8 +15,7 @@ export const fetchAnime = async (page: number) => {
   const data = await response.json();
 
   // Ensure all items have a unique `id`
-  return data.map((anime: any, index: number) => ({
-    ...anime,
-    id: anime.id || `${page}-${index}`, // Generate a fallback ID if missing
-  }));
+  return data.map((item: AnimeProp, index: number) => (
+    <AnimeCard key={item.id} anime={item} index={index} />
+  ));
 };
